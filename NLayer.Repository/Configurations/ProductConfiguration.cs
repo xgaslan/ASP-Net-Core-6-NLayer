e4cbc6757acb67f4a094794410b818d69e0dba71
xgaslan <gorkem_aslan0@hotmail.com>
2022-03-05 23:47:03 +0300

    DbContext and configuration files has been created

---
+++ NLayer.Repository/Configurations/ProductConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core.Models;
+
+namespace NLayer.Repository.Configurations;
+
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.HasKey(p => p.Id);
+        builder.Property(p => p.Id).UseIdentityColumn();
+        builder.Property(p => p.Name).IsRequired().HasMaxLength(200);
+        builder.Property(p => p.Stock).IsRequired();
+        builder.Property(p => p.Price).IsRequired().HasColumnType("decimal(18, 2)");
+        builder.ToTable("Products");
+
+        builder.HasOne(p => p.Category)
+            .WithMany(c => c.Products)
+            .HasForeignKey(p => p.CategoryId);
+    }
+}
---
+++ NLayer.Repository/Configurations/ProductFeatureConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core.Models;
+
+namespace NLayer.Repository.Configurations;
+
+public class ProductFeatureConfiguration : IEntityTypeConfiguration<ProductFeature>
+{
+    public void Configure(EntityTypeBuilder<ProductFeature> builder)
+    {
+        builder.HasKey(pf => pf.Id);
+        builder.Property(pf => pf.Id).UseIdentityColumn();
+
+        builder.HasOne(pf => pf.Product)
+            .WithOne(p => p.ProductFeature)
+            .HasForeignKey<ProductFeature>(pf => pf.ProductId);
+    }
+}
