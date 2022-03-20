2eac2b76b6f74418eb7d0aed3439a118a42eedb1
xgaslan <gorkem_aslan0@hotmail.com>
2022-03-20 19:00:23 +0300

    Repositories has been created.

---
+++ NLayer.Repository/Repositories/CategoryRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core.Models;
+using NLayer.Core.Repositories;
+
+namespace NLayer.Repository.Repositories;
+
+public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
+{
+    public CategoryRepository(AppDbContext context) : base(context)
+    {
+    }
+
+    public async Task<List<Category>> GetCategoriesWithProducts()
+    {
+        var result = await _context.Categories
+            .Include(c => c.Products).ToListAsync();
+        return result;
+    }
+
+    public async Task<Category> GetCategoryByIdWithProducts(int id)
+    {
+        var result = await _context.Categories
+            .Include(c => c.Products)
+            .SingleOrDefaultAsync(c => c.Id == id);
+        return result;
+    }
+}
---
+++ NLayer.Repository/Repositories/ProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core.Models;
+using NLayer.Core.Repositories;
+
+namespace NLayer.Repository.Repositories;
+
+public class ProductRepository : GenericRepository<Product>, IProductRepository
+
+{
+    public ProductRepository(AppDbContext context) : base(context)
+    {
+    }
+
+    public async Task<List<Product>> GetProductsWithCategory()
+    {
+        var result = await _context.Products
+            .Include(p => p.Category).ToListAsync();
+        return result;
+    }
+
+    public async Task<Product> GetProductByIdWithCategory(int id)
+    {
+        var result = await _context.Products
+                .Include(p => p.Category)
+                .SingleOrDefaultAsync(p => p.Id == id);
+        return result;
+    }
+}
